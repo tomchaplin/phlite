e08a120b901f84f77f5ee07438b3437373dde5d3
Thomas Chaplin <tomrchaplin@gmail.com>
2024-04-07 08:31:48 +0100

    Correct coefficient in reduction algo + maintain `r_col`

--- src/reduction.rs
+++ src/reduction.rs
@@ -1,65 +1,80 @@
 use std::collections::HashMap;
+use std::fmt::Debug;
 use std::hash::Hash;
 
 use crate::{
+    columns::ColumnEntry,
     fields::{Invertible, NonZeroCoefficient},
-    matricies::{product, FiniteOrderedColBasis, HasRowFiltration, MatrixRef, VecVecMatrix},
+    matricies::{FiniteOrderedColBasis, HasRowFiltration, MatrixOracle, VecVecMatrix},
 };
 
 // TODO:
 // 1. Convert to oracle
 // 2. Implement clearing
-// 3. Don't keep dropping and rebuilding r_col
 
-pub fn inefficient_reduction<M: MatrixRef + FiniteOrderedColBasis>(
-    boundary: M,
-) -> VecVecMatrix<'static, M::CoefficientField, usize>
+pub fn inefficient_reduction<M>(boundary: M) -> VecVecMatrix<'static, M::CoefficientField, usize>
 where
-    M: MatrixRef + HasRowFiltration,
+    M: MatrixOracle + FiniteOrderedColBasis + HasRowFiltration,
     M::CoefficientField: Invertible,
     M::RowT: Hash,
+    M::RowT: Debug,
 {
     let mut v = vec![];
     for i in 0..boundary.n_cols() {
         v.push(vec![(M::CoefficientField::one(), i)]);
     }
 
-    let mut low_inverse: HashMap<M::RowT, usize> = HashMap::new();
+    // low_inverse[i]=(j, lambda) means R[j] has lowest non-zero in row i with coefficient lambda
+    let mut low_inverse: HashMap<M::RowT, (usize, M::CoefficientField)> = HashMap::new();
 
-    for i in 0..boundary.n_cols() {
+    'column_loop: for i in 0..boundary.n_cols() {
         // Reduce column i
-        loop {
-            // Build up temporary oracle for product
-            let v_oracle = VecVecMatrix::from(&v);
-            // Build up r_col from the product
-            let mut r_col = product(boundary, &v_oracle).build_bhcol(i).unwrap();
 
-            // Check if we have a pivot to reduce
+        let mut r_col = boundary.build_bhcol(i).unwrap();
+
+        'reduction: loop {
             let Some(pivot_entry) = r_col.pop_pivot() else {
-                break;
+                // Column reduced to 0 -> found cycle -> move onto next column
+                continue 'column_loop;
             };
+
+            let pivot_row_index = pivot_entry.row_index;
+            let pivot_coeff = pivot_entry.coeff;
+
+            // Push the pivot back in to keep r_col coorect
+            r_col.push(pivot_entry);
 
             // Check if there is a column with the same pivot
-            let Some(col_idx_to_add) = low_inverse.get(&pivot_entry.row_index) else {
-                break;
+            let Some((other_col_idx, other_col_coeff)) = low_inverse.get(&pivot_row_index) else {
+                // Cannot reduce further -> found boundary -> break and save pivot
+                break 'reduction;
             };
 
-            // If so then we add -1/coeff with the corresponding index to V
-            let col_multiple = pivot_entry.coeff.inverse().additive_inverse();
+            // If so then we add a multiple of that column to cancel out the pivot in r_col
+            let col_multiple = pivot_coeff.additive_inverse() * (other_col_coeff.inverse());
 
-            v[i].push((col_multiple, *col_idx_to_add))
+            // Add the multiple of that column
+            r_col.add_entries(
+                boundary
+                    .column_with_filtration(*other_col_idx)
+                    .unwrap()
+                    .map(|e| e.unwrap())
+                    .map(|entry| {
+                        ColumnEntry::from((
+                            entry.coeff * col_multiple,
+                            entry.row_index,
+                            entry.filtration_value,
+                        ))
+                    }),
+            );
+
+            // Update V
+            v[i].push((col_multiple, *other_col_idx))
         }
 
-        // Keep low_inverse up to date
-
-        // Build up temporary oracle for product
-        let v_oracle = VecVecMatrix::from(&v);
-        // Build up r_col from the product
-        let mut r_col = product(boundary, &v_oracle).build_bhcol(i).unwrap();
-
-        // Check if we have a pivot to reduce
+        // Save pivot if we have one
         if let Some(pivot_entry) = r_col.pop_pivot() {
-            low_inverse.insert(pivot_entry.row_index, i);
+            low_inverse.insert(pivot_entry.row_index, (i, pivot_entry.coeff));
         };
     }
 
@@ -68,6 +83,7 @@ where
 
 #[cfg(test)]
 mod tests {
+
     use crate::matricies::{product, simple_Z2_matrix, MatrixOracle, MatrixRef};
 
     use super::inefficient_reduction;
@@ -84,6 +100,8 @@ mod tests {
             vec![3, 4, 5],
             vec![3, 4, 5],
         ]);
+        let matrix_v = inefficient_reduction(matrix_d.with_trivial_filtration());
+        let matrix_r = product(&matrix_d, &matrix_v);
         let true_matrix_r = simple_Z2_matrix(vec![
             vec![],
             vec![],
@@ -94,12 +112,6 @@ mod tests {
             vec![3, 4, 5],
             vec![],
         ]);
-        let matrix_d_with_basis = matrix_d.with_trivial_filtration();
-
-        let matrix_v = inefficient_reduction(&matrix_d_with_basis);
-
-        let matrix_r = product(&matrix_d_with_basis, &matrix_v);
-
-        assert!((0..=6).all(|idx| matrix_r.eq_on_col(&true_matrix_r, idx)))
+        assert!((0..=7).all(|idx| matrix_r.eq_on_col(&true_matrix_r, idx)))
     }
 }
